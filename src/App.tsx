51178ee1e69662e663a5921d9f039e875597aecb
GuilhermeFRocha <guilhermefdarocha@outlook.com>
2023-08-29 22:12:47 -0300

    create form

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,62 @@
-import { Input } from "./Input";
+import { useState, useEffect } from "react";
 
-export function App() {
+export const App = () => {
+  const [selectedDateInit, setSelectedInitDate] = useState("");
+  const [selectedDateEnd, setSelectedEndDate] = useState("");
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function fetchVendasData() {
+      try {
+        const response = await fetch("https://data.origamid.dev/vendas/");
+
+        if (!response.ok) {
+          throw new Error("Erro ao buscar os dados da API");
+        }
+
+        const data = await response.json();
+        setData(data);
+      } catch (error) {
+        console.error("Ocorreu um erro:", error);
+      }
+    }
+
+    fetchVendasData();
+  }, []);
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const handleDateChange = (event: any) => {
+    setSelectedInitDate(event.target.value);
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const handleDateEndChange = (event: any) => {
+    setSelectedEndDate(event.target.value);
+  };
+
   return (
     <>
       <div>
-        <Input nameLabel="name" htmlFor="name" name="name" id="name" />
+        <label htmlFor="datePicker">Escolha uma data:</label>
+        <input
+          type="date"
+          id="datePicker"
+          value={selectedDateInit}
+          onChange={handleDateChange}
+        />
+        <p>Data selecionada: {selectedDateInit}</p>
+      </div>
+
+      <div>
+        <label htmlFor="datePicker">Escolha uma data:</label>
+        <input
+          type="date"
+          id="datePicker"
+          value={selectedDateEnd}
+          onChange={handleDateEndChange}
+        />
+        <p>Data selecionada: {selectedDateEnd}</p>
       </div>
     </>
   );
-}
+};
