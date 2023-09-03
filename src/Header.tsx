e2c463bb3a8e2f97481a2344c9d7a292d3a53f71
GuilhermeFRocha <guilhermefdarocha@outlook.com>
2023-09-03 13:38:17 -0300

    create context

--- src/Header.tsx
+++ src/Header.tsx
@@ -5,7 +5,9 @@ export const Header = () => {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const { dark, setDark }: any = useContext(UiContext);
 
-  console.log(dark);
-
-  return <div>Header</div>;
+  return (
+    <div>
+      <button onClick={() => setDark(!dark)}>{dark ? "dark" : "light"}</button>
+    </div>
+  );
 };
