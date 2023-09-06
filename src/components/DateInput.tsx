896fc525aa2cb7ce7cdcffa3b347c501e1605260
GuilhermeFRocha <guilhermefdarocha@outlook.com>
2023-09-05 21:40:56 -0300

    add style

--- src/components/DateInput.tsx
+++ src/components/DateInput.tsx
@@ -1,4 +1,25 @@
-import { ComponentProps } from "react";
+import { ComponentProps, CSSProperties } from "react";
+
+const labelStyle: CSSProperties = {
+  display: "block",
+  marginBottom: "var(--gap-s)",
+  fontWeight: 600,
+  fontSize: "1rem",
+  color: "var(--color-2)",
+  padding: "var(--gap-s) .75rem",
+  backgroundColor: "var(--color-4)",
+  borderRadius: "var(--gap)",
+};
+
+const inputStyle: CSSProperties = {
+  border: "none",
+  fontFamily: "monospace",
+  fontSize: "1rem",
+  color: "var(--color-2)",
+  padding: "var(--gap-s) .75rem",
+  backgroundColor: "var(--color-4)",
+  borderRadius: "var(--gap)",
+};
 
 interface IDateInput extends ComponentProps<"input"> {
   label: string;
@@ -7,8 +28,16 @@ interface IDateInput extends ComponentProps<"input"> {
 export const DateInput = ({ label, ...props }: IDateInput) => {
   return (
     <div>
-      <label htmlFor={label}>{label}</label>
-      <input type="date" name={label} id={label} />
+      <label style={labelStyle} htmlFor={label}>
+        {label}
+      </label>
+      <input
+        style={inputStyle}
+        type="date"
+        name={label}
+        id={label}
+        {...props}
+      />
     </div>
   );
 };
--- src/components/DateRange.tsx
+++ src/components/DateRange.tsx
@@ -1,10 +1,25 @@
+import { useState } from "react";
 import { DateInput } from "./DateInput";
 
 export const DateRange = () => {
+  const [inicio, setInicio] = useState("");
+  const [final, setFinal] = useState("");
   return (
-    <div>
-      <DateInput label="Inicio" />
-      <DateInput label="Final" />
-    </div>
+    <form className="box flex" onSubmit={(e) => e.preventDefault()}>
+      <DateInput
+        label="Inicio"
+        value={inicio}
+        onChange={({ target }) => {
+          setInicio(target.value);
+        }}
+      />
+      <DateInput
+        label="Final"
+        value={final}
+        onChange={({ target }) => {
+          setFinal(target.value);
+        }}
+      />
+    </form>
   );
 };
