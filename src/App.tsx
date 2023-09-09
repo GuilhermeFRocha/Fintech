894fcad80ccbaf024a3bf74a92ba78deed13a14a
GuilhermeFRocha <guilhermefdarocha@outlook.com>
2023-09-09 19:42:19 -0300

    create sidenav

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,28 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { DataContextProvider } from "./Context/DataContext";
 import { Header } from "./components/Header";
 import { Sidenav } from "./components/Sidenav";
 import Sales from "./pages/Sales";
 import { Summary } from "./pages/Summary";
 import "./style.css";
+import Sale from "./pages/Sale";
 
 export const App = () => {
   return (
-    <DataContextProvider>
-      <div className="container">
-        <Sidenav />
-        <main>
-          <Header />
-          <Summary />
-          <Sales />
-        </main>
-      </div>
-    </DataContextProvider>
+    <BrowserRouter>
+      <DataContextProvider>
+        <div className="container">
+          <Sidenav />
+          <main>
+            <Header />
+            <Routes>
+              <Route path="/" element={<Summary />} />
+              <Route path="/vendas" element={<Sales />} />
+              <Route path="/vendas/:id" element={<Sale />} />
+            </Routes>
+          </main>
+        </div>
+      </DataContextProvider>
+    </BrowserRouter>
   );
 };
